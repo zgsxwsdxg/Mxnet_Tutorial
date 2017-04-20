60912782233fa387c1590281f9eda44b462d0485
jonggon <medical18@naver.com>
2017-04-21 00:43:09 +0900

    fully_connected_gan_completed

--- applications/Generative Adversarial Network version2 with Convolution Neural Network/Network.py
+++ applications/Generative Adversarial Network version2 with Convolution Neural Network/Network.py
@@ -32,42 +32,39 @@ def Data_Processing(batch_size):
 
     '''data loading referenced by Data Loading API '''
     train_iter = mx.io.NDArrayIter(data={'data': to2d(train_img)}, batch_size=batch_size, shuffle=True)  # training data
-
-    return train_iter
+    return train_iter,len(train_img)
 
 def Generator():
 
     #generator neural networks
     noise = mx.sym.Variable('noise') # The size of noise is 128.
-    #g_affine1 = mx.sym.FullyConnected(data=noise,name='g_affine1',num_hidden=256)
-    #generator1 = mx.sym.Activation(data=g_affine1, name='g_sigmoid1', act_type='sigmoid')
-    g_affine2 = mx.sym.FullyConnected(data=noise, name='g_affine2', num_hidden=256)
-    generator2= mx.sym.Activation(data=g_affine2, name='g_sigmoid2', act_type='sigmoid')
-    g_affine3 = mx.sym.FullyConnected(data=generator2, name='g_affine3', num_hidden=784)
-    g_out= mx.sym.Activation(data=g_affine3, name='g_sigmoid3', act_type='sigmoid')
+    g_affine1 = mx.sym.FullyConnected(data=noise, name='g_affine1', num_hidden=256)
+    generator1= mx.sym.Activation(data=g_affine1, name='g_sigmoid1', act_type='sigmoid')
+    g_affine2 = mx.sym.FullyConnected(data=generator1, name='g_affine2', num_hidden=784)
+    g_out= mx.sym.Activation(data=g_affine2, name='g_sigmoid2', act_type='sigmoid')
     return g_out
 
 def Discriminator():
 
     #discriminator neural networks
     data = mx.sym.Variable('data') # The size of data is 784(28*28)
-
-    #d_affine1 = mx.sym.FullyConnected(data=data,name='d_affine1',num_hidden=512)
-    #discriminator1 = mx.sym.Activation(data=d_affine1,name='d_sigmoid1',act_type='sigmoid')
-    d_affine2 = mx.sym.FullyConnected(data=data,name = 'd_affine2' , num_hidden=256)
+    d_affine1 = mx.sym.FullyConnected(data=data,name = 'd_affine1' , num_hidden=256)
+    discriminator1 = mx.sym.Activation(data=d_affine1, name='d_sigmoid1', act_type='sigmoid')
+    d_affine2 = mx.sym.FullyConnected(data=discriminator1,name = 'd_affine2' , num_hidden=128)
     discriminator2 = mx.sym.Activation(data=d_affine2, name='d_sigmoid2', act_type='sigmoid')
-    d_affine3 = mx.sym.FullyConnected(data=discriminator2,name = 'd_affine3' , num_hidden=128)
-    discriminator3 = mx.sym.Activation(data=d_affine3, name='d_sigmoid3', act_type='sigmoid')
-    d_affine4 = mx.sym.FullyConnected(data=discriminator3,name = 'd_affine4' , num_hidden=1)
-    discriminator4 = mx.sym.Activation(data=d_affine4, name='d_sigmoid4', act_type='sigmoid')
+    d_affine3 = mx.sym.FullyConnected(data=discriminator2,name = 'd_affine3' , num_hidden=1)
+    d_out = mx.sym.Activation(data=d_affine3, name='d_sigmoid3', act_type='sigmoid')
 
     '''expression-1'''
-    #out1 = mx.sym.MakeLoss(mx.symbol.log(discriminator4),grad_scale=-1.0,name="loss1")
-    #out2 = mx.sym.MakeLoss(mx.symbol.log(1.0-discriminator4),grad_scale=-1.0,name='loss2')
+    #out1 = mx.sym.MakeLoss(mx.symbol.log(discriminator3),grad_scale=-1.0,name="loss1")
+    #out2 = mx.sym.MakeLoss(mx.symbol.log(1.0-discriminator3),grad_scale=-1.0,name='loss2')
 
-    '''expression-2'''
-    out1 = mx.sym.MakeLoss(-mx.symbol.log(discriminator4),name="loss1")
-    out2 = mx.sym.MakeLoss(-mx.symbol.log(1.0-discriminator4),name='loss2')
+    '''expression-2,
+    question? Why multiply the loss equation by -1?
+    answer : for Maximizing the Loss function , and This is because mxnet only provides optimization techniques that minimize.
+    '''
+    out1 = mx.sym.MakeLoss(-1.0*mx.symbol.log(d_out),grad_scale=1.0,normalization='batch',name="loss1")
+    out2 = mx.sym.MakeLoss(-1.0*mx.symbol.log(1.0-d_out),grad_scale=1.0,normalization='batch',name='loss2')
 
     group=mx.sym.Group([out1,out2])
 
@@ -77,7 +74,7 @@ def GAN(epoch,noise_size,batch_size,save_period):
 
     save_weights=True
     save_path="Weights/"
-    train_iter= Data_Processing(batch_size)
+    train_iter,train_data_number= Data_Processing(batch_size)
     noise_iter = NoiseIter(batch_size, noise_size)
 
     column_size=10
@@ -87,9 +84,9 @@ def GAN(epoch,noise_size,batch_size,save_period):
     Generative Adversarial Networks
 
     <structure>
-    generator(size = 128) - 512 - 128 - 256 - (size = 784 : image generate)
+    generator(size = 128) - 256 - (size = 784 : image generate)
 
-    discriminator(size = 784) -  512 - 256 - (size=1 : Identifies whether the image is an actual image or not)
+    discriminator(size = 784) - 256  - 128 - (size=1 : Identifies whether the image is an actual image or not)
 
     cost_function - MIN_MAX cost_function
     '''
@@ -104,14 +101,20 @@ def GAN(epoch,noise_size,batch_size,save_period):
     # =============module G=============
     modG = mx.mod.Module(symbol=generator, data_names=['noise'], label_names=None, context= mx.gpu(0))
     modG.bind(data_shapes=noise_iter.provide_data,label_shapes=None,for_training=True)
-    modG.init_params(initializer=mx.init.Normal(0.02))
+
+    #modG.load_params(save_path+"modG-100.params")
+
+    modG.init_params(initializer=mx.initializer.Xavier(rnd_type='uniform', factor_type='avg', magnitude=3))
     modG.init_optimizer(optimizer='adam',optimizer_params={'learning_rate': 0.01})
 
 
     # =============module discriminator[0],discriminator[1]=============
     modD_0 = mx.mod.Module(symbol=discriminator[0], data_names=['data'], label_names=None, context= mx.gpu(0))
     modD_0.bind(data_shapes=train_iter.provide_data,label_shapes=None,for_training=True,inputs_need_grad=True)
-    modD_0.init_params(initializer=mx.init.Normal(0.02))
+
+    #modD_0.load_params(save_path+"modD_0-100.params")
+
+    modD_0.init_params(initializer=mx.initializer.Xavier(rnd_type='uniform', factor_type='avg', magnitude=3))
     modD_0.init_optimizer(optimizer='adam',optimizer_params={'learning_rate': 0.01})
 
     modD_1 = mx.mod.Module(symbol=discriminator[1], data_names=['data'], label_names=None, context= mx.gpu(0))
@@ -132,17 +135,23 @@ def GAN(epoch,noise_size,batch_size,save_period):
     ####################################training loop############################################
     # =============train===============
     for epoch in xrange(1,epoch+1,1):
+        Max_cost_0=0
+        Max_cost_1=0
+        Min_cost=0
+        total_batch_number = np.ceil(train_data_number/(batch_size*1.0))
         print "epoch : {}".format(epoch)
         train_iter.reset()
         null.reset()
         for batch in train_iter:
-
             ################################updating only parameters related to modD.########################################
             # updating discriminator on real data
             '''MAX : modD_0 : -mx.symbol.log(discriminator2)  real data Discriminator update , bigger and bigger discriminator2'''
             modD_0.forward(batch, is_train=True)
             modD_0.backward()
             modD_0.update()
+
+            '''Max_Cost of real data Discriminator'''
+            Max_cost_0-=modD_0.get_outputs()[0].asnumpy()
 
             # update discriminator on noise data
             '''MAX : modD_1 :-mx.symbol.log(1-discriminator2)  - noise data Discriminator update , bigger and bigger -> smaller and smaller discriminator2'''
@@ -154,6 +163,9 @@ def GAN(epoch,noise_size,batch_size,save_period):
             modD_1.backward()
             modD_1.update()
 
+            '''Max_Cost of noise data Discriminator'''
+            Max_cost_1-=modD_0.get_outputs()[0].asnumpy()
+
             ################################updating only parameters related to modG.########################################
             # update generator on noise data
             '''MIN : modD_0 : -mx.symbol.log(discriminator2) - noise data Discriminator update  , bigger and bigger discriminator2'''
@@ -163,9 +175,18 @@ def GAN(epoch,noise_size,batch_size,save_period):
             modG.backward(diff_v)
             modG.update()
 
+            '''Max_Cost of noise data Generator'''
+            Min_cost-=modG.get_outputs()[0].asnumpy()
+
             '''No need, but must be declared.'''
             null.update([label], modD_0.get_outputs())
 
+        Max_C=(Max_cost_0+Max_cost_1)/(total_batch_number*1.0)
+        Min_C=Max_cost_0/(total_batch_number*1.0)
+
+        #cost print
+        print "Max Discriminator Cost : {}".format(Max_C.mean())
+        print "Min Generator Cost : {}".format(Min_C.mean())
 
         #Save the data
         if save_weights and epoch%save_period==0:
@@ -206,9 +227,7 @@ def GAN(epoch,noise_size,batch_size,save_period):
     result = test_mod.get_outputs()[0]
     print result
     result = result.asnumpy()
-    #print result
 
-    #'''
     #'''
     #visualization
     #fig ,  ax = plt.subplots(int(row_size/2.0), column_size, figsize=(column_size, int(row_size/2.0)))
@@ -223,15 +242,15 @@ def GAN(epoch,noise_size,batch_size,save_period):
         #show 20 image
         ax[0][i].set_axis_off()
         ax[1][i].set_axis_off()
-        ax[0][i].imshow(np.reshape(result[i], (28, 28)))
-        ax[1][i].imshow(np.reshape(result[i+10], (28, 28)))
+        ax[0][i].imshow(np.reshape(result[i], (28, 28)),cmap='gray')
+        ax[1][i].imshow(np.reshape(result[i+10], (28, 28)),cmap='gray')
     plt.show()
     #'''
 
 if __name__ == "__main__":
 
     print "GAN_starting in main"
-    GAN(epoch=2, noise_size=128, batch_size=100, save_period=100)
+    GAN(epoch=300, noise_size=128, batch_size=100, save_period=300)
 
 else:
 
--- applications/Generative Adversarial Network version2 with Convolution Neural Network/main.py
+++ applications/Generative Adversarial Network version2 with Convolution Neural Network/main.py
@@ -1,4 +1,4 @@
 import Network
 
 '''I wrote this code with reference to  https://github.com/dmlc/mxnet/blob/master/example/gan/dcgan.py.'''
-Network.GAN(epoch=300,noise_size=128,batch_size=100,save_period=300)
+Network.GAN(epoch=300, noise_size=128, batch_size=100, save_period=300)
